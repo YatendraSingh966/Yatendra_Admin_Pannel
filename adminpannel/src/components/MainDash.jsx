aba5a1cdb553e0fce67b34c9a4b2f02d6883540d
Yatendra_Singh <yatendrasingh966@gmail.com>
2023-09-14 00:42:09 +0530

    Created Cards Layout

--- adminpannel/src/components/MainDash.jsx
+++ adminpannel/src/components/MainDash.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Cards from "./Cards";
 
 export default function MainDash() {
   return (
     <div className="MainDash">
       <h1>Dashboard</h1>
+      <Cards />
     </div>
   );
 }
